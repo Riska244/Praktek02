b8b26e67f44a954065c6cf1bd996b766d5e3e6d1
riska <riskafawn@gmail.com>
2023-05-27 14:51:44 +0700

    Method tanpa nilai balik

--- src/main/java/praktek02/RectangleAksi.java
+++ src/main/java/praktek02/RectangleAksi.java
@@ -8,7 +8,8 @@ public class RectangleAksi {
         rl.lebar = 3;
         
         rl.cetakinfo();
-        System.out.println("Luas rectangle ="+rl.hitungLuas())
+        System.out.println("Luas rectangle ="+rl.hitungLuas());
+        rl.cetakLuas();
     }
     
 }
