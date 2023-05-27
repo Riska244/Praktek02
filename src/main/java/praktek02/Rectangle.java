9e798366d397d6380f454062d2d76f707b6999ce
riska <riskafawn@gmail.com>
2023-05-27 14:56:53 +0700

    Construkctor - sama dengan nama class

--- src/main/java/praktek02/Rectangle.java
+++ src/main/java/praktek02/Rectangle.java
@@ -3,11 +3,27 @@ package praktek02;
 public class Rectangle {
     double panjang;
     double lebar;
+
+    public Rectangle() {
+        panjang=5;
+        lebar=2;
+    }
+        
     
     void cetakinfo() {
         System.out.println("================");
         System.out.println("Panjang :"+panjang);
         System.out.println("Lebar   :"+lebar);
         System.out.println("================");
+    }
+    
+    double hitungLuas() {
+        double luas;
+        luas=panjang+lebar;
+        return luas;
+    }
+    
+    void cetakLuas() {
+        System.out.println("Luas adalah: "+hitungLuas());
     }
 }
